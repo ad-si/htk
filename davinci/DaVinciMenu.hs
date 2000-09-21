500d90baefc7036b2decb725e62694fffd8fc90f
ger <ger@56da9416-c632-0410-a62c-99bba40abcbd>
2000-09-21 22:00:44 +0000

    Some sanitisation of DaVinciMenu
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@136 56da9416-c632-0410-a62c-99bba40abcbd

--- davinci/DaVinciMenu.hs
+++ davinci/DaVinciMenu.hs
@@ -23,15 +23,15 @@ TO BE DONE    : Accelerators
 
 module DaVinciMenu (
 
-        Graph,
-
-        Menu,
-        installGraphMenu,
-        installNodeTypeMenu,
-        installEdgeTypeMenu
-
-           
-        ) where
+   Graph,
+   
+   Menu,
+   installGraphMenu,
+   installNodeTypeMenu,
+   installEdgeTypeMenu
+   
+      
+   ) where
 
 import SIM
 import GUICore
@@ -93,8 +93,8 @@ installNodeTypeMenu mn g tid assocs = do {
 toMenuType :: GUIOBJECT -> IO MenuType
 toMenuType guio = do {
         t <- getText guio;
-        acc <-  getAccelerator guio;
-        mne <-  getUnderline guio;
+        acc <-  getAccelerator guio; -- unused
+        mne <-  getUnderline guio;  -- unused
         kind <- getObjectKind guio;
         makeMenu guio kind (show (objectID guio)) t acc mne 
         }
@@ -118,69 +118,104 @@ makeMenu guio MENUBUTTON oid t acc mne = do {
 
 
 -- ---------------------------------------------------------------------------
--- The Correwsponding DaVinci Commands
+-- DaVinci commands for activating and creating menus.
+-- (Only used for Graph menus.)
 -- ---------------------------------------------------------------------------
 
 activateMenus :: [MenuType] -> String
-activateMenus mt = "app_menu(activate_menus("++ show (getMenuIds mt) ++"))"
+-- This activates all the menus.  It's only necessary for Graph's.
+activateMenus menus = 
+   callDaVinci "app_menu" [
+      callDaVinci "activate_menus" [
+         show (getMenuIds menus)
+         ]]
         
 getMenuIds :: [MenuType] -> [MenuId]
 getMenuIds [] = []
-getMenuIds ((MenuType oid _ _ _):l) = oid : getMenuIds l
-getMenuIds ((SubMenuType oid _ tl _):l) = oid : (getMenuIds tl ++ getMenuIds l)
-getMenuIds (Blank : l) = getMenuIds l
+getMenuIds ((MenuType menuId _ _ _):rest) = menuId : getMenuIds rest
+getMenuIds ((SubMenuType menuId _ menus _):rest) = 
+   menuId : (getMenuIds menus ++ getMenuIds rest)
+getMenuIds (Blank : rest) = getMenuIds rest
 
 createMenus :: [MenuType] -> String
-createMenus x = ("app_menu(create_menus(" ++ show x ++ "))")
+createMenus x =
+   "("++ 
+      (callDaVinci "app_menu" [show x]) 
+      ++  ")"
 
 -- ---------------------------------------------------------------------------
 -- DaVinci Menu Definitions
 -- ---------------------------------------------------------------------------
 
 data MenuType = 
-          MenuType MenuId MenuLbl MenuMne MenuAcc
-        | SubMenuType MenuId MenuLbl [MenuType] MenuMne
-        | Blank
+      MenuType MenuId MenuLabel MenuMne MenuAcc
+      -- a single button. 
+   |  SubMenuType MenuId MenuLabel [MenuType] MenuMne
+      -- a submenu
+   |  Blank
+      -- a gap (left on a menu to separate menu items)
 
 type MenuId     =  String
+-- For MenuType the MenuId is the string sent by daVinci when the menu 
+-- button is pressed.  For both MenuType and SubMenuType, the MenuId
+-- can be used to disable the item.
 
-data MenuMod    = NoMenuMod | MenuMod Modifier                  
+type MenuLabel    = String
+-- The MenuLabel is the string displayed to the user on the menu.
+
+-- For MenuType MenuMod and MenuMod (if set, and they should both be
+-- set if either is set) give a keyboard shortcut.
+
+data MenuMod    = NoMenuMod | MenuMod Modifier
+-- A Modifier is described in htk/resources/GUIEvent.hs.  It is Control,
+-- Shift, Meta or Alt.  It is the key to be pressed with a MenuAcc
+-- to shortcut the menu.
+-- Unused.
 
 type MenuAcc    = Maybe (MenuMod,String)
-
-type MenuLbl    = String
+-- The String in MenuAcc should have precisely one character.
+-- This is always Nothing
 
 type MenuMne    = Maybe String
+-- This is a "Motif mnemonic" whatever that is.  It's always Nothing.
+
 
 -- ---------------------------------------------------------------------------
 -- Unparsing of DaVinci Menu Definitions
 -- ---------------------------------------------------------------------------
 
 instance Show MenuType where
-        showsPrec d  (MenuType mid mlbl Nothing Nothing) r =
-                "menu_entry("++ show mid++comma++ show mlbl++")" 
-                ++ r
-        showsPrec d  (MenuType mid mlbl (Just mne) (Just (mmod,macc))) r=
-                "menu_entry_mne(" ++ show mid++comma++ show mlbl ++comma++
-                show mne ++comma++ show mmod ++comma++ show macc ++")"
-                ++ r
-        showsPrec d (SubMenuType mid mlbl mtypes Nothing) r =
-                "submenu_entry(" ++ show mid++comma++ show mlbl ++comma++
-                show mtypes ++")"
-                ++ r    
-        showsPrec d (SubMenuType mid mlbl mtypes (Just mne)) r =
-                "submenu_entry_mne(" ++ show mid++comma++
-                show mlbl ++comma++ show mtypes ++ comma ++ show mne    ++")" 
-                ++ r
-        showsPrec d Blank r = "blank" ++ r
+   showsPrec d  (MenuType menuId menuLabel Nothing Nothing) =
+      callDaVinciAcc "menu_entry" [show menuId,show menuLabel]
+   showsPrec d  (MenuType menuId menuLabel (Just mne) (Just (mmod,macc))) =
+      callDaVinciAcc "menu_entry_mne" 
+         [show menuId,show menuLabel,show mne,show mmod,show macc]
+   showsPrec d (SubMenuType menuId menuLabel mtypes Nothing) =
+      callDaVinciAcc "submenu_entry"
+         [show menuId,show menuLabel,show mtypes]
+   showsPrec d (SubMenuType menuId menuLabel mtypes (Just mne)) =
+      callDaVinciAcc "submenu_entry_mne"
+         [show menuId,show menuLabel,show mtypes,show mne]
+   showsPrec d Blank = ("blank"++)
 
+-- callDaVinci and callDaVinciAcc unparses the standard DaVinci function call.
+-- callDaVinciAcc allows you to add a parameter to append at the end.
+callDaVinci :: String -> [String] -> String
+callDaVinci funName funArgs = callDaVinciAcc funName funArgs ""
+
+callDaVinciAcc :: String -> [String] -> String -> String
+callDaVinciAcc funName funArgs toAppend =
+   let
+      withCommas [] = ""
+      withCommas [one] = one
+      withCommas (first:rest) = first ++ "," ++ withCommas rest
+   in
+      funName ++ "(" ++ withCommas funArgs ++ ")" ++ toAppend
 
 instance Show MenuMod where
-        showsPrec d  (MenuMod Alt)  r           = "alt" ++ r
-        showsPrec d  (MenuMod Shift)  r         = "shift" ++ r
-        showsPrec d  (MenuMod Control) r        = "control" ++ r
-        showsPrec d  (MenuMod Meta) r           = "meta" ++ r
-        showsPrec d  NoMenuMod  r               = "none" ++ r
+   showsPrec d  (MenuMod Alt)  r           = "alt" ++ r
+   showsPrec d  (MenuMod Shift)  r         = "shift" ++ r
+   showsPrec d  (MenuMod Control) r        = "control" ++ r
+   showsPrec d  (MenuMod Meta) r           = "meta" ++ r
+   showsPrec d  NoMenuMod  r               = "none" ++ r
 
-
-comma      = "," 
