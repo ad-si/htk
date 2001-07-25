5f40b50b4709ceec96c728061e0478b9b5d8d8d1
3till <3till@56da9416-c632-0410-a62c-99bba40abcbd>
2001-07-25 14:21:54 +0000

    *** empty log message ***
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@373 56da9416-c632-0410-a62c-99bba40abcbd

---
+++ graphs/test/TestTest.hs
@@ -0,0 +1,85 @@
+
+module Main(main) where
+
+
+{- Test file for graph package. -}
+
+import Concurrent
+
+import Debug(debug)
+
+import Selective
+import SIM(Destructible(..),lift)
+
+import GraphDisp
+import GraphConfigure
+
+import DaVinciGraphDisp
+import SIM(shutdown)
+import WBFiles(parseArguments)
+
+   
+setUpGraph 
+   (displaySort ::
+       GraphDisp.Graph graph graphParms node nodeType nodeTypeParms arc 
+          arcType arcTypeParms) =
+   do
+
+      -- Create new graph
+
+      let
+         graphParms  = 
+            GraphTitle "Test Graph Display" $$
+            OptimiseLayout True $$ 
+            emptyGraphParms 
+      graph <- newGraph displaySort graphParms
+
+      (killChannel :: Channel ()) <- newChannel
+
+
+      -- Setup node and egde types
+
+      let
+         (nodeType1Parms :: nodeTypeParms ()) =
+            LocalMenu (Menu Nothing []) $$$ emptyNodeTypeParms
+
+         arcType1Parms = LocalMenu (Menu Nothing [])  
+                         $$$ emptyArcTypeParms
+
+      nodeType1 <- newNodeType graph nodeType1Parms
+
+      arcType1 <- newArcType graph arcType1Parms
+
+
+      -- Create nodes and edges
+
+      nodeA <- newNode graph nodeType1 ()
+      nodeB <- newNode graph nodeType1 ()
+      nodeC <- newNode graph nodeType1 ()
+
+      arcA <- newArc graph arcType1 "Arc A" nodeA nodeB
+      arcB <- newArc graph arcType1 "Arc B" nodeB nodeC
+      arcC <- newArc graph arcType1 "" nodeC nodeA
+
+
+      -- Draw graph
+
+      redraw graph
+
+
+      -- Exit
+
+      sync(
+            (lift(receive killChannel) >>> 
+               do
+                  putStrLn "Destroy graph"
+                  destroy graph
+               )
+         +> (destroyed graph)
+         )
+
+main = 
+   do
+      parseArguments
+      setUpGraph daVinciSort
+      shutdown
