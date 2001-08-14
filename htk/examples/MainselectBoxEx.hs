8009f08c6cb8ebb356a49b7f060a669657d7987b
ludi <ludi@56da9416-c632-0410-a62c-99bba40abcbd>
2001-08-14 22:05:45 +0000

    uniform grand unification (2nd)
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@393 56da9416-c632-0410-a62c-99bba40abcbd

---
+++ htk/examples/MainselectBoxEx.hs
@@ -0,0 +1,40 @@
+-- -----------------------------------------------------------------------
+--
+-- $Source$
+--
+-- HTk - a GUI toolkit for Haskell  -  (c) Universitaet Bremen
+--
+-- $Revision$ from $Date$
+-- Last modification by $Author$
+--
+-- -----------------------------------------------------------------------
+
+module Main (main) where
+
+import HTk
+import SelectBox
+
+main:: IO ()
+main =
+  do
+    main <- initHTk [text "select box example"]
+
+    sb1 <- newSelectBox main Nothing []
+    pack sb1 [Fill X, Expand On, Side AtTop]
+
+    b1_1 <- addButton sb1 [text "Button1.1"] [] :: IO (Button String)
+    b2_1 <- addButton sb1 [text "Button2.1"] [] :: IO (Button String)
+    addSpace sb1 10
+    b3_1 <- addButton sb1 [text "Button3.1"] [] :: IO (Button String)
+    b4_1 <- addButton sb1 [text "Button4.1"] [] :: IO (Button String)
+
+    sb2 <- newSelectBox main (Just 2) []
+    pack sb2 [Fill X, Expand On, Side AtBottom]
+
+    b1_2 <- addButton sb2 [text "Button1.2"] [] :: IO (Button String)
+    b2_2 <- addButton sb2 [text "Default Button"] [] :: IO (Button String)
+    b3_2 <- addButton sb2 [text "Button3.2"] [] :: IO (Button String)
+    b4_2 <- addButton sb2 [text "Button4.2"] [] :: IO (Button String)
+
+    (htk_destr, _) <- bindSimple main Destroy
+    sync (htk_destr)
