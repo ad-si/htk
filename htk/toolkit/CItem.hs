312bbc81cc7c5b67022a6e610f0ceabf648840cd
ludi <ludi@56da9416-c632-0410-a62c-99bba40abcbd>
2001-09-12 21:00:24 +0000

    forgot this
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@425 56da9416-c632-0410-a62c-99bba40abcbd

---
+++ htk/toolkit/CItem.hs
@@ -0,0 +1,28 @@
+-- -----------------------------------------------------------------------
+--
+-- $Source$
+--
+-- HTk - a GUI toolkit for Haskell  -  (c) Universitaet Bremen
+--
+-- $Revision$ from $Date$
+-- Last modification by $Author$
+--
+-- -----------------------------------------------------------------------
+
+module CItem (
+
+  CItem(..)
+
+) where
+
+import HTk
+import Name
+
+
+------------------------------------------------------------
+-- class CItem collects all properties items need to have --
+------------------------------------------------------------
+
+class {- Eq c => -} CItem c where
+  getName :: c -> IO Name
+  getIcon :: c -> IO Image
