a117ceb82fc777191fb4b2257552bbf7ca149e3c
mclee <mclee@56da9416-c632-0410-a62c-99bba40abcbd>
2001-09-27 08:01:23 +0000

    *** empty log message ***
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@443 56da9416-c632-0410-a62c-99bba40abcbd

--- htk/examples/MainmodalDialogTest.hs
+++ htk/examples/MainmodalDialogTest.hs
@@ -7,6 +7,8 @@ import MarkupText
 import InputWin
 import InputForm
 
+data Test = Test {ent1 :: String, ent2 :: String}  
+
 main :: IO ()
 main =
  do
@@ -80,12 +82,18 @@ main =
 
   clickedbut9 <- clicked but9
   spawnEvent (forever (clickedbut9 >> always (do 
+                                               let def = Test{ent1="HALLO WELT!\n rofl", ent2="TEST"}
                                                iwin <- newInputWin "HELLO THERE" []
-					       form <- newInputForm (iwin # fForm) []
-					       newEntryField form [text "entry 1"] :: IO (EntryField String String)
-					       newEntryField form [text "entry 2"] :: IO (EntryField String String)
+					       form <- newInputForm (iwin # fForm) (Just def) []
+					       newTextField form [size (5,5),selector ent1, 
+					                           modifier (\ old val -> old {ent1=val})] :: IO (TextField Test String)
+					       newTextField form [size (5,5),selector ent2, modifier (\ old val -> (old {ent2=val}))] :: IO (TextField Test String)
+					       setFormValue form def
 					       res <- wait iwin True
-                                               putStrLn(show(res))					       
+                                               putStrLn(show(res))
+					       def2 <- getFormValue form
+					       putStrLn(show(def2 # ent1)++" "++show(def2 # ent2))
+					       putStrLn(show(def # ent1)++" "++show(def # ent2))
 					       )))
  
   clickedbut2 <- clicked but2
