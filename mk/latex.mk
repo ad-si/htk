e170c0d28fce0289adf6e914de56d3668c211dc9
cxl <cxl@56da9416-c632-0410-a62c-99bba40abcbd>
2002-04-14 22:51:51 +0000

    Rules for LaTeX and friends.
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@653 56da9416-c632-0410-a62c-99bba40abcbd

---
+++ mk/latex.mk
@@ -0,0 +1,35 @@
+
+# Rules to handle LaTex sources
+#
+# Strangely, this may not work with Solaris make-- use GNU make instead.
+
+.SUFFIXES: .tex .bib .fig .eps .bbl .ps .pdf .dvi
+
+FIG2DEV = fig2dev
+LATEX   = latex
+PDFLATEX = pdflatex
+BIBTEX  = bibtex
+DVIPS   = dvips
+
+.fig.eps:
+	$(FIG2DEV) -L ps $< > $@
+
+.fig.pdf:
+	$(FIG2DEV) -L pdf $< > $@
+
+.dvi.ps:
+	$(DVIPS) -o $@ $<
+
+.tex.pdf: 
+	$(PDFLATEX) $<; $(PDFLATEX) $<
+
+# This will fall over if we did not run LaTeX on $< at least once to
+# create a .aux file...
+.bib.bbl:
+	$(LATEX) $<; $(BIBTEX) $*
+
+.tex.dvi:
+	$(LATEX) $<; $(LATEX) $<
+
+#
+
