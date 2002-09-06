c38fefbb104a7a6e2b9d610f04aa8a797a0894ff
ger <ger@56da9416-c632-0410-a62c-99bba40abcbd>
2002-09-06 11:46:36 +0000

    Stop SysVars.hs being compiled on Solaris (the relevant function only seems to be available on Linux anyway).
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@817 56da9416-c632-0410-a62c-99bba40abcbd

--- posixutil/SysVars.hs
+++ posixutil/SysVars.hs
@@ -1,4 +1,7 @@
-{- Access to environment variables not provided elsewhere -}
+{- Access to environment variables not provided elsewhere.
+
+   This module only works on Linux so far as I know.  It is only
+   compiled when we are working around the BDB Linux bug.  -}
 module SysVars(
    unSetEnv, -- :: String -> IO ()
       -- delete an environment variable from the process's environment.
