d9cd51254d23525226d3e670d69180b0864fbf5e
ger <ger@56da9416-c632-0410-a62c-99bba40abcbd>
2003-02-19 13:52:33 +0000

    Add a test case
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@951 56da9416-c632-0410-a62c-99bba40abcbd

---
+++ posixutil/test/ChildProcessTest.hs
@@ -0,0 +1,28 @@
+{- Function which runs a program, given as its argument, passing stdin to it,
+   and printing the program's stdout. -}
+module ChildProcessTest where
+
+import Concurrent
+
+import ChildProcess
+
+runProg :: String -> IO ()
+runProg progName =
+   do
+      child <- newChildProcess progName []
+      forkIO (passOutput child)
+      passInput child
+
+passInput :: ChildProcess -> IO ()
+passInput child =
+   do
+      input <- getLine
+      sendMsg child input 
+      passInput child
+
+passOutput :: ChildProcess -> IO ()
+passOutput child =
+   do
+      output <- readMsg child
+      putStrLn ("Got: "++output)
+      passOutput child
