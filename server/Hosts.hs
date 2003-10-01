089fe79ab32493a5a3b42ded35a074d23fd8b87d
ger <ger@56da9416-c632-0410-a62c-99bba40abcbd>
2003-10-01 11:01:24 +0000

    Add Hosts.hs so the user doesn't need DtdToHaskell
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@1102 56da9416-c632-0410-a62c-99bba40abcbd

---
+++ server/Hosts.hs
@@ -0,0 +1,49 @@
+module DTD_Hosts where
+
+import Xml2Haskell
+
+
+{-Type decls-}
+
+newtype Hosts = Hosts [Host] 		deriving (Eq , Show)
+data Host = Host
+    { hostHostName :: String
+    , hostPort :: (Defaultable String)
+    , hostDescription :: (Maybe String)
+    , hostUser :: (Maybe String)
+    } deriving (Eq , Show)
+
+
+{-Instance decls-}
+
+instance XmlContent Hosts where
+    fromElem (CElem (Elem "hosts" [] c0):rest) =
+	(\(a,ca)->
+	   (Just (Hosts a), rest))
+	(many fromElem c0)
+    fromElem rest = (Nothing, rest)
+    toElem (Hosts a) =
+	[CElem (Elem "hosts" [] (concatMap toElem a))]
+instance XmlContent Host where
+    fromElem (CElem (Elem "host" as []):rest) =
+	(Just (fromAttrs as), rest)
+    fromElem rest = (Nothing, rest)
+    toElem as =
+	[CElem (Elem "host" (toAttrs as) [])]
+instance XmlAttributes Host where
+    fromAttrs as =
+	Host
+	  { hostHostName = definiteA fromAttrToStr "host" "hostName" as
+	  , hostPort = defaultA fromAttrToStr "11393" "port" as
+	  , hostDescription = possibleA fromAttrToStr "description" as
+	  , hostUser = possibleA fromAttrToStr "user" as
+	  }
+    toAttrs v = catMaybes 
+	[ toAttrFrStr "hostName" (hostHostName v)
+	, defaultToAttr toAttrFrStr "port" (hostPort v)
+	, maybeToAttr toAttrFrStr "description" (hostDescription v)
+	, maybeToAttr toAttrFrStr "user" (hostUser v)
+	]
+
+
+{-Done-}
