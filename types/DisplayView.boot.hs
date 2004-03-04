afe75be20442b7d95ebe78b314ac194a91c3ec81
ger <ger@56da9416-c632-0410-a62c-99bba40abcbd>
2004-03-04 08:44:26 +0000

    Change to use kind annotations.
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@1234 56da9416-c632-0410-a62c-99bba40abcbd

--- types/DisplayView.boot.hs
+++ types/DisplayView.boot.hs
@@ -1,23 +1,7 @@
 {- This is a boot version of DisplayView. -}
 module DisplayView where
 
-#if (__GLASGOW_HASKELL__ >= 503)
-#define PrelInt GHC.Base.Int
-#else
-#define PrelInt Int
-#endif
-
 -- We need to at least force the kinds of the type variables to be right.
-data  DisplayedView graph graphParms node nodeType nodeTypeParms arc arcType 
-      arcTypeParms =
-   NOTHING {
-      graph :: graph,
-      graphParms :: graphParms,
-      node :: node PrelInt,
-      nodeType :: nodeType PrelInt,
-      nodeTypeParms :: nodeTypeParms PrelInt,
-      arc :: arc PrelInt,
-      arcType :: arcType PrelInt,
-      arcTypeParms :: arcTypeParms PrelInt
-      }
-
+data  DisplayedView graph graphParms 
+   (node :: * -> *) (nodeType :: * -> *) (nodeTypeParms :: * -> *)
+   (arc :: * -> *) (arcType :: * -> *) (arcTypeParms :: * -> *)
