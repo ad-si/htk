d3faba1b9789d0ede3859f38345b56b4d29a5070
maeder <maeder@56da9416-c632-0410-a62c-99bba40abcbd>
2008-09-10 11:47:31 +0000

    rename MMiSS files and replace module names
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@1637 56da9416-c632-0410-a62c-99bba40abcbd

--- mk/ChangeFoldernames.sh
+++ mk/ChangeFoldernames.sh
@@ -2,6 +2,7 @@
 
 createDir ()
 {
+echo creating $1/$2 and copying sources
 cd $1
 mkdir -p $2
 cp *.hs $2
@@ -9,55 +10,71 @@ cd ..
 }
 
 mkdir -p appl/Appl
-createDir davinci/ UDrawGraph
-createDir emacs/ Emacs
-createDir events/ Events
-createDir graphs/ Graphs
+createDir davinci UDrawGraph
+createDir emacs Emacs
+createDir events Events
+createDir graphs Graphs
 mkdir -p htk/Htk
-createDir imports/ Imports
-createDir mmiss/ MMiSS
-createDir posixutil/ Posixutil
-createDir reactor/ Reactor
-createDir server/ Server
-createDir simpledb/  Simpledb
-createDir types/ Types
-createDir util/ Util
+createDir imports Imports
+createDir mmiss MMiSS
+createDir posixutil Posixutil
+createDir reactor Reactor
+createDir server Server
+createDir simpledb  Simpledb
+createDir types Types
+createDir util Util
 
 createSubDir ()
 {
+echo creating $1/$3 and copying sources
 rm -rf $1/$3
 cp -r $2 $1/$3
 }
 
 cd htk
-createSubDir Htk canvasitems/ Canvasitems/
-createSubDir Htk components/ Components/
-createSubDir Htk containers/ Containers/
-createSubDir Htk devices/ Devices/
-createSubDir Htk kernel/ Kernel/
-createSubDir Htk menuitems/ Menuitems/
-createSubDir Htk textitems/ Textitems/
-createSubDir Htk tix/ Tix/
-createSubDir Htk toolkit/ Toolkit/
-createSubDir Htk toplevel/ Toplevel/
-createSubDir Htk widgets/ Widgets/
+createSubDir Htk canvasitems Canvasitems
+createSubDir Htk components Components
+createSubDir Htk containers Containers
+createSubDir Htk devices Devices
+createSubDir Htk kernel Kernel
+createSubDir Htk menuitems Menuitems
+createSubDir Htk textitems Textitems
+createSubDir Htk tix Tix
+createSubDir Htk toolkit Toolkit
+createSubDir Htk toplevel Toplevel
+createSubDir Htk widgets Widgets
 cd ..
 
 cd appl
-createSubDir Appl ontologytool/ Ontologytool/
+createSubDir Appl ontologytool Ontologytool
 cd ..
 
 cd mmiss
-createSubDir MMiSS api/ Api/
-createSubDir MMiSS parser/ Parser/
+createSubDir MMiSS api Api
+createSubDir MMiSS parser Parser
 cd ..
 
-cd davinci/UDrawGraph
-for file in DaVinci*.hs
+renameHierFile ()
+{
+echo renaming $1 files
+for file in $1*.hs
 do
    echo $file > temp
-   newfile=`sed -e "s/DaVinci//" temp`
+   newfile=`sed -e "s/$1//" temp`
    cp -f $file $newfile
    rm -f $file temp
 done
+}
+
+cd davinci/UDrawGraph
+renameHierFile DaVinci
 cd ../..
+
+cd mmiss/MMiSS
+renameHierFile MMiSS
+cd Api
+renameHierFile MMiSS
+cd ../../..
+
+mk/ReplaceModuleNames */[A-Z]*/*.hs
+mk/ReplaceModuleNames */[A-Z]*/[A-Z]*/*.hs
