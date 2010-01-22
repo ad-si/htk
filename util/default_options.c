04e173b658a7e9c1a180ff8a38c9e87a25e5528a
maeder <maeder@56da9416-c632-0410-a62c-99bba40abcbd>
2010-01-22 18:11:48 +0000

    do not use configured options
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@1706 56da9416-c632-0410-a62c-99bba40abcbd

--- util/default_options.c
+++ util/default_options.c
@@ -1,6 +1,9 @@
 /* default_options is a string representing the default
    options to be compiled into UniForM. */
+/* This is a version to be compiled into uni when exported, at least until
+   we have a better way of doing things.   What we can't guess, we leave
+   blank. */
 #include "default_options.h"
-#include "uni-config.h"
 
-const char default_options[] = DEFAULTOPTIONS;
+const char default_options[] =
+"--uni-wish:wish --uni-daVinci:uDrawGraph";
