db6f6666cb11566067a06ea79d6af8a6c59ebbb0
maeder <maeder@56da9416-c632-0410-a62c-99bba40abcbd>
2010-09-29 15:14:23 +0000

    unused or moved to util or simpledb
    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/uni/trunk@1728 56da9416-c632-0410-a62c-99bba40abcbd

--- includes/bdbcommon.h
+++
@@ -1,8 +0,0 @@
-#include "db.h"
-
-/* Print a message */
-void mess(const char *fmt,...);
-
-/* Check the return value ret from a BDB operation called "name", and if it is
-   set display an error message */
-void run_db(const char *name,int ret);
